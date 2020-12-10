590edc06631e99bde3bab1e8c3d4112ef06fd2df
sfofana <58987175+sfofana@users.noreply.github.com>
2020-12-09 19:12:25 -0500

    add subject service

---
+++ bank/src/main/resources/static/bankApp/src/app/services/subject.service.ts
@@ -0,0 +1,21 @@
+import { BehaviorSubject, Subject } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { AccountHolder } from '../models/account-holder';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubjectService {
+
+  blankHolder: AccountHolder = {} as AccountHolder
+
+  public unsunscribe = new Subject();
+
+  private behavior = new BehaviorSubject<AccountHolder>(this.blankHolder);
+
+  public setSession(holder: AccountHolder){
+    this.behavior.next(holder);
+  }
+
+  public session = this.behavior.asObservable();
+}
